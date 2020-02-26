7b9f752bace46300b368a092dc859ff9e12dded8
Jack Teversham <jacktevers@gmail.com>
2020-02-26 12:29:58 +0200

    Dimension extraction from prefix.data file working

--- VolImage.cpp
+++ VolImage.cpp
@@ -1,29 +1,32 @@
 
+#include "VolImage.h"
 #include "VolImageMan.cpp"
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <fstream>
+
+
+
 
 
 int main(int argc, char ** argv){
-
     //DEAL WITH COMMAND LINE ARGUMENTS
      string prefix;
      string output_filename;
      bool diffMap;
      bool extract;
      int i, j;
-     std::istringstream iss;
-    if(!(argc >= 2)){
+    
+    if(!(argc >= 2)){ //the file prefix is compulsory
         cout << "\nINVALID ARGUMENTS.\nexiting...\n\n";
              return 0; //exit the program
     }
 
-     if (argc > 2){
+     if (argc > 2){           //NOTE: CAN ONLY DO ONE INVOCATION OF EITHER -d OR -x EACH RUN
          prefix = argv[1];
-         string option;
-         iss.str(argv[2]); //convert char sequence to string for comparision
-         iss >> option;
+
+         string option = string(argv[2]);
         
          if(option=="-d"){
              diffMap = true;
@@ -36,17 +39,55 @@ int main(int argc, char ** argv){
              i = stoi(argv[3]); //convert string input to int
              j = 0;
              output_filename = argv[4];
-         } else {
-
+         } else { //if option is -d or -x then invalid arguments and exit
              cout << "\nINVALID ARGUMENTS.\nexiting...\n\n";
              return 0; //exit the program
-         }
+             }  
          
      }else{
          prefix = argv[1];
-     }
+         diffMap = false;
+         extract = false;
+     }  
+     //END COMMAND LINE INTEFRACE
+     int width, height, stacks;
+     string filename = prefix +".data";
+     readHeader(width, height, stacks, filename);
+
+
 
 
 
     return 0;
 }
+
+void readHeader(int width, int height, int stacks, string filename){
+    string line;
+    ifstream ifs;
+   
+    ifs.open("../raws/"+filename); //try to open file and catch error
+    if(!ifs){
+        cerr<<"\nfile open failed.\nexiting...";
+        exit(0);
+    }else{
+        getline(ifs, line, '\n');
+        //cout << line << endl;
+
+        vector<int> dim;
+        string token;
+        istringstream ss(line); //crete a string stream with the line of dimensions
+        while (getline(ss, token, ' '))
+        {
+            dim.push_back(stoi(token));
+        }
+        //assign dimensions
+        width = dim[0];
+        height = dim[1];
+        stacks = dim[2];
+
+        cout << width << height << stacks << endl;
+        
+    }
+}
+
+
---
+++ VolImage.h
@@ -0,0 +1,8 @@
+#ifndef VOLIMAGE_H
+#define VOLIMAGE_H
+
+#include <string>
+using namespace std;
+void readHeader(int width, int height, int stacks, string filename);
+
+#endif
